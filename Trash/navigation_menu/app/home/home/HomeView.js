ada08a1548dfadf411809808d1a42e576fff61b0
angelo.falci@mail.polimi.it <agosto1994>
2017-11-09 00:43:06 +0100

    First upload

---
+++ Trash/navigation_menu/app/home/home/HomeView.js
@@ -0,0 +1,10 @@
+const HomeViewModel = require("./home-view-model");
+
+function onLoaded(args) {
+	
+}
+function onTap(eventData) {
+	
+  console.info("Changes are saved!");
+}
+exports.onTap = onTap;
