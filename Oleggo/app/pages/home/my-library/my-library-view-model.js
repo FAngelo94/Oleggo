3a9b9b24936913adf4e5553e06ddafe4f23fa64c
angelo.falci@mail.polimi.it <agosto1994>
2017-11-11 14:57:25 +0100

    list books set up

--- Oleggo/app/pages/home/my-library/my-library-view-model.js
+++ Oleggo/app/pages/home/my-library/my-library-view-model.js
@@ -1,8 +1,70 @@
 const observableModule = require("data/observable");
+const ObservableArray = require("data/observable-array").ObservableArray;
 
 function MyLibraryViewModel() {
     const viewModel = observableModule.fromObject({
-
+		BookList: new ObservableArray(
+        [{
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            }
+        ])
     });
 
     return viewModel;
