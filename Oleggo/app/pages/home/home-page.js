67ac42cb32d233b2f7ae2efcd85874f464d600f0
Julian Gallego <julian.gallego@upb.edu.co>
2017-11-11 12:53:19 +0100

    library

--- Oleggo/app/pages/home/home-page.js
+++ Oleggo/app/pages/home/home-page.js
@@ -1,6 +1,79 @@
 const frameModule = require("ui/frame");
 
 const HomeViewModel = require("./home-view-model");
+const observableModule = require("data/observable");
+const ObservableArray = require("data/observable-array").ObservableArray;
+let page;
+let pageData = new observableModule.fromObject({
+    BookList: new ObservableArray(
+        [{
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            }
+        ])
+});
+
+exports.loaded = function (args) {
+    page = args.object;
+    page.bindingContext = pageData;
+};
+
 
 /* ***********************************************************
 * Use the "onNavigatingTo" handler to initialize the page binding context.
--- Oleggo/app/pages/home/my-library/my-library.js
+++ Oleggo/app/pages/home/my-library/my-library.js
@@ -1,6 +1,73 @@
-const SearchViewModel = require("./my-library-view-model");
+/* const observableModule = require("data/observable");
+const ObservableArray = require("data/observable-array").ObservableArray;
+let page;
+let pageData = new observableModule.fromObject({
+    BookList: new ObservableArray(
+        [{
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "eggs",
+                author: "julian",
+                image: "res://breakfast1"
+            },
+            {
+                title: "bread",
+                author: "david",
+                image: "~/images/empty.png"
+            },
+            {
+                title: "cereal",
+                author: "gallego",
+                image: "res://book"
+            }
+        ])
+});
 
-function onLoaded(args) {
-    
-}
-
+exports.loaded = function (args) {
+    page = args.object;
+    page.bindingContext = pageData;
+};
+ */
