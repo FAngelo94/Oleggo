b225f810e9bc110a2e1f41ceeafa0f6983c23651
Julian Gallego <julian.gallego@upb.edu.co>
2017-11-20 15:31:52 +0100

    word definition functionality

--- Oleggo/app/pages/add-note/add-note-page.js
+++ Oleggo/app/pages/add-note/add-note-page.js
@@ -8,28 +8,28 @@ var SpeechRecognition = require("nativescript-speech-recognition").SpeechRecogni
 var speechRecognition = new SpeechRecognition();
 // import the options
 var SpeechRecognitionTranscription = require("nativescript-speech-recognition").SpeechRecognitionTranscription;
-
+var wd = require("word-definition")
 /* ***********************************************************
-* Use the "onNavigatingTo" handler to initialize the page binding context.
-*************************************************************/
+ * Use the "onNavigatingTo" handler to initialize the page binding context.
+ *************************************************************/
 function onNavigatingTo(args) {
     /* ***********************************************************
-    * The "onNavigatingTo" event handler lets you detect if the user navigated with a back button.
-    * Skipping the re-initialization on back navigation means the user will see the
-    * page in the same data state that he left it in before navigating.
-    *************************************************************/
+     * The "onNavigatingTo" event handler lets you detect if the user navigated with a back button.
+     * Skipping the re-initialization on back navigation means the user will see the
+     * page in the same data state that he left it in before navigating.
+     *************************************************************/
     if (args.isBackNavigation) {
         return;
     }
 
     const page = args.object;
-	label=page.getViewById("speakText");
+    label = page.getViewById("speakText");
     page.bindingContext = new AddNoteViewModel();
-	speechRecognition.available().then(
-	  function(available) {
-		console.log(available ? "YES!" : "NO");
-	  }
-	);
+    speechRecognition.available().then(
+        function (available) {
+            console.log(available ? "YES!" : "NO");
+        }
+    );
 }
 exports.onNavigatingTo = onNavigatingTo;
 /* ***********************************************************
@@ -43,30 +43,31 @@ function onDrawerButtonTap(args) {
 }
 exports.onDrawerButtonTap = onDrawerButtonTap;
 
-function listen(args){
-	console.info("listening...");
-	speechRecognition.startListening(
-	{
-		returnPartialResults: true,
-		// this callback will be invoked repeatedly during recognition
+function listen(args) {
+    console.info("listening...");
+    speechRecognition.startListening({
+        returnPartialResults: true,
+        // this callback will be invoked repeatedly during recognition
         onResult: function (transcription) {
             console.info("User said: " + transcription.text);
             console.info("User finished?: " + transcription.finished);
-			if(transcription.finished==true)
-			{
-				label.text=transcription.text;
-			}
+            if (transcription.finished == true) {
+                label.text = transcription.text;
+            }
         },
-	});
+    });
 }
 exports.listen = listen;
 
-function addQuote(args){
-	console.info(label.text);
+function addQuote(args) {
+    console.info(label.text);
 }
 exports.addQuote = addQuote;
 
-function lookForWord(args){
-	console.info(label.text);
+function lookForWord(args) {
+    console.info(label.text);
+    wd.getDef("Car", "en", null, (definition) => {
+        console.log(definition.definition);
+    });
 }
 exports.lookForWord = lookForWord;
